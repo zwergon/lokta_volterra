a1f6013d407412f81138f198d4af856977fbbe75
zwergon <zwergon@free.fr>
2022-09-18 21:37:18 +0200

    add setup and one more article (sde numerical)

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='LoktaVolterra',
+      version='1.0',
+      description='Module to understand SDE',
+      author='Zwergon',
+      author_email='zwergon@free.fr',
+      packages=['pub_crawl', 'spark_volterra', 'stats', 'tests'],
+     )
